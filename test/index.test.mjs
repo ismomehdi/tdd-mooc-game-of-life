8a017d1b6ee448095dc4dd724a5b1f3d25153f89
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-05 19:57:19 +0200

    Better test description

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -3,13 +3,15 @@ import { expect } from "chai";
 import { execSync } from "child_process";
 
 describe("CLI", () => {
-  test("outputs an error when no argument is provided", () => {
-    const output = execSync("node ./src/index.mjs").toString();
-    expect(output).toBe("Error: No argument provided or too many arguments.\n");
-  });
+  describe("input error handling", () => {
+    test("outputs an error when no argument is provided", () => {
+      const output = execSync("node ./src/index.mjs").toString();
+      expect(output).toBe("Error: No argument provided or too many arguments.\n");
+    });
 
-  test("outputs an error when provided more than one argument", () => {
-    const output = execSync("node ./src/index.mjs 1 2").toString();
-    expect(output).toBe("Error: No argument provided or too many arguments.\n");
+    test("outputs an error when provided more than one argument", () => {
+      const output = execSync("node ./src/index.mjs 1 2").toString();
+      expect(output).toBe("Error: No argument provided or too many arguments.\n");
+    });
   });
 });
