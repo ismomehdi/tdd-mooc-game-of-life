fa7ec2cd36003e444ea0381de0ae1e6cf5dd73f4
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-05 19:54:32 +0200

    Add test for checking if there's more than 1 argument provided

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 const arg = process.argv[2];
 
 if (parseInt(process.argv.length) != 3) {
-  console.log("Error: No argument provided");
+  console.log("Error: No argument provided or too many arguments.");
 } else {
 }
--- test/index.test.mjs
+++ test/index.test.mjs
@@ -5,6 +5,11 @@ import { execSync } from "child_process";
 describe("CLI", () => {
   test("outputs an error when no argument is provided", () => {
     const output = execSync("node ./src/index.mjs").toString();
-    expect(output).toBe("Error: No argument provided\n");
+    expect(output).toBe("Error: No argument provided or too many arguments.\n");
+  });
+
+  test("outputs an error when provided more than one argument", () => {
+    const output = execSync("node ./src/index.mjs 1 2").toString();
+    expect(output).toBe("Error: No argument provided or too many arguments.\n");
   });
 });
