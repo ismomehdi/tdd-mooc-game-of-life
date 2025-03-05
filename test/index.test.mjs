3ebce881d4431aacfd8991f120665b5582cdef57
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-05 19:39:57 +0200

    Add initial test for the CLI tool

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -2,8 +2,17 @@ import { describe, test } from "vitest";
 import { expect } from "chai";
 import { sum } from "../src/index.mjs";
 
+import { execSync } from "child_process";
+
 describe("Example test fixture", () => {
   test("Example test", () => {
     expect(sum(1, 2)).to.equal(3);
+  });
+});
+
+describe("CLI", () => {
+  test('prints "hello world"', () => {
+    const output = execSync("node ./src/index.mjs").toString();
+    expect(output).to.equal("hello world\n");
   });
 });
