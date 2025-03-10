55d35cdb54b0b8869628d9c4a05fd64fe219143e
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-10 16:51:02 +0200

    shrinkGrid(): remove ALL empty rows/columns at the borders of grid

--- src/lib/generate.mjs
+++ src/lib/generate.mjs
@@ -1,3 +1,5 @@
+export function generate(grid) {}
+
 export function extendGrid(grid) {
   const newGrid = grid.map((row) => [0, ...row, 0]);
   const emptyRow = new Array(newGrid[0].length).fill(0);
@@ -5,13 +7,9 @@ export function extendGrid(grid) {
 }
 
 export function shrinkGrid(grid) {
-  // Remove empty rows
-  if (grid[0].every((cell) => cell === 0)) grid.shift();
-  if (grid[grid.length - 1].every((cell) => cell === 0)) grid.pop();
-
-  // Remove empty columns
-  if (grid.every((row) => row[0] === 0)) grid.forEach((row) => row.shift());
-  if (grid.every((row) => row[row.length - 1] === 0)) grid.forEach((row) => row.pop());
-
+  while (grid[0].every((cell) => cell === 0)) grid.shift();
+  while (grid[grid.length - 1].every((cell) => cell === 0)) grid.pop();
+  while (grid.every((row) => row[0] === 0)) grid.forEach((row) => row.shift());
+  while (grid.every((row) => row[row.length - 1] === 0)) grid.forEach((row) => row.pop());
   return grid;
 }
--- test/generate.test.mjs
+++ test/generate.test.mjs
@@ -21,11 +21,13 @@ describe("run a generation", () => {
   });
 
   describe("shrinkGrid()", () => {
-    test("removes empty rows and columns at the borders of the grid", () => {
+    test("removes all empty rows and columns at the borders of the grid", () => {
       const grid = [
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
       ];
       const expected = [[1], [1], [1]];
       expect(shrinkGrid(grid)).toEqual(expected);
@@ -35,9 +37,9 @@ describe("run a generation", () => {
   // todo
   describe("generate()", () => {
     test.skip("any live cell with fewer than two live neighbours dies", () => {
-      const grid = [[1, 1, 1]];
-      const expected = [[1], [1], [1]];
-      throw new Error("Not implemented");
+      const grid = [[1, 1]];
+      const expected = [];
+      expect(generate(grid).toEqual(expected));
     });
   });
 });
